10564034683b0d321f0a5a777747d414bd1fc456
Conrado Fonseca <conradob@live.com>
2020-07-10 09:50:10 +0100

    Pass cancellation token instead of cancellation source to execute function of retry policy

--- index.js
+++ index.js
@@ -6,29 +6,33 @@ const failCallback = () =>
   )
 
 const main = async () => {
-  const retryPolicy = Policy.handleAll()
-    .retry()
-    .exponential({ maxDelay: 10 * 1000 })
+  try {
+    const retryPolicy = Policy.handleAll()
+      .retry()
+      .exponential({ maxDelay: 10 * 1000 })
 
-  const cancellationTokenSource = new CancellationTokenSource()
+    const cancellationTokenSource = new CancellationTokenSource()
 
-  let count = 0
-  retryPolicy.onRetry(() => {
-    ++count
+    let count = 0
+    retryPolicy.onRetry(() => {
+      ++count
 
-    console.log('retrying', count)
-    if (count > 5) {
-      console.log('canceling at', count)
-      cancellationTokenSource.cancel()
-    }
-  })
+      console.log('retrying', count)
+      if (count > 5) {
+        console.log('canceling at', count)
+        cancellationTokenSource.cancel()
+      }
+    })
 
-  const result = await retryPolicy.execute(
-    () => failCallback(),
-    cancellationTokenSource
-  )
+    const result = await retryPolicy.execute(
+      () => failCallback(),
+      cancellationTokenSource.token
+    )
 
-  console.log(result)
+    console.log(result)
+  } catch (error) {
+    console.error(error)
+  }
 }
 
 main()
